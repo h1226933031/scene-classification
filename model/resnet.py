4b7b4722297a02400acbd570f4e9e21d18805402
h1226933031 <yuehan.wang@163.com>
2023-04-16 22:06:08 +0800

    add resnet and attention

---
+++ model/resnet.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+
+
+class BasicBlock(nn.Module):
+    # residual function
+    def __init__(self, in_c, out_c, kernel_size):  # stride is fixed to 1
+        super(BasicBlock, self).__init__()
+        assert kernel_size % 2 == 1, "kernel_size is not single"
+        # set padding to adjust the output size to match the dimension
+        self.n_pad = kernel_size // 2  # n_pad = ((stride-1)*input_size + kernel_size - stride) // 2
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_c, out_c, kernel_size=kernel_size, stride=1, padding=self.n_pad, bias=False),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_c, out_c, kernel_size=kernel_size, stride=1, padding=self.n_pad, bias=False),
+            nn.BatchNorm2d(out_c)
+        )
+        self.shortcut = nn.Sequential()
+        if in_c != out_c:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_c, out_c, kernel_size=kernel_size, stride=1, padding=self.n_pad, bias=False),
+                nn.BatchNorm2d(out_c)
+            )
+
+    def forward(self, x):
+        res = self.residual_function(x)
+        shortcut = self.shortcut(x)
+        return nn.ReLU(inplace=True)(res + shortcut)
+
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        input_channels = [1, 4, 16, 32, 64, 128, 128]
+        output_sizes = [128, 64, 8]
+        kernel_sizes = [7, 5, 3]
+
+        assert len(output_sizes) == len(kernel_sizes) and len(input_channels) == 2 * len(kernel_sizes) + 1
+
+        self.res_cov_function = nn.Sequential()
+
+        for i, (o, k) in enumerate(zip(output_sizes, kernel_sizes)):
+            self.res_cov_function.append(BasicBlock(in_c=input_channels[2*i], out_c=input_channels[2*i+1], kernel_size=k))
+            self.res_cov_function.append(nn.Conv2d(input_channels[2*i+1], input_channels[2*i+2], kernel_size=k, padding=1))
+            self.res_cov_function.append(nn.ReLU())
+            self.res_cov_function.append(nn.AdaptiveMaxPool2d((o, o)))
+
+        # Fully connected layer
+        self.fc = nn.Linear(input_channels[-1] * output_sizes[-1] ** 2, 15)
+
+    def forward(self, x):
+        x = self.res_cov_function(x)
+        x = x.view(x.size(0), -1)  # flatten
+        return self.fc(x)  # [batch_size, num_class=15]
+
+
+if __name__ == '__main__':
+    inputs = torch.randn(8, 1, 224, 224)
+    net = ResNet()
+    outputs = net(inputs)
--- run.py
+++ run.py
@@ -4,26 +4,15 @@ from load_data import read_data, Dataset_scene
 import torch
 import torch.nn as nn
 from model.CNN import CNN_3_layers
+from model.resnet import ResNet
+from model.attention import Attention
 from utils.train import train_one_epoch, vali
+import argparse
 
 
-def main():
+def main(args):
     fix_seed = 666
     torch.manual_seed(fix_seed)
-
-    # dataset parameters
-    data_path = './train/'
-    augment = True
-    labels = {'bedroom': 1, 'Coast': 2, 'Forest': 3, 'Highway': 4, 'industrial': 5, 'Insidecity': 6,
-              'kitchen': 7, 'livingroom': 8, 'Mountain': 9, 'Office': 10, 'OpenCountry': 11, 'store': 12,
-              'Street': 13, 'Suburb': 14, 'TallBuilding': 15}
-    labels = dict([(k, v - 1) for k, v in labels.items()])
-
-    # training parameters
-    MAX_EPOCH = 5
-    BATCH_SIZE = 128
-    LR = 0.01
-    best_model_path = './ckpt/'
 
     # enable devices
     if torch.cuda.is_available():
@@ -33,37 +22,58 @@ def main():
         device = torch.device("cpu")
         print('Use CPU')
 
-    train_data, val_data = read_data(data_path, labels, val_ratio=0.2, seed=fix_seed)
+    train_data, val_data = read_data(args.data_path, labels, val_ratio=args.val_ratio, seed=fix_seed)
     print('#(train_data):', len(train_data))
     print('#(val_data):', len(val_data))
 
-    train_set = Dataset_scene(train_data, augment=augment)
-    val_set = Dataset_scene(val_data, augment=augment)
+    train_set = Dataset_scene(train_data, augment=args.data_augmentation)
+    val_set = Dataset_scene(val_data, augment=args.data_augmentation)
 
-    train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(dataset=val_set, batch_size=BATCH_SIZE, shuffle=False)
+    train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=args.batch_size, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(dataset=val_set, batch_size=args.batch_size, shuffle=False)
 
-    model = CNN_3_layers().to(device)
+    model_dict = {'CNN_3_layers': CNN_3_layers, 'ResNet': ResNet, 'Attention:': Attention}
+    model = model_dict[args.model_name]()
+    model.to(device)
     criterion = nn.CrossEntropyLoss().to(device)
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=LR, momentum=0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
     best_val_loss = np.Inf
-    if not os.path.exists(best_model_path):
-        os.makedirs(best_model_path)
+    if not os.path.exists(args.ckpt_path):
+        os.makedirs(args.ckpt_path)
 
-    for epoch in range(MAX_EPOCH):
+    for epoch in range(args.epochs):
         train_loss, train_acc = train_one_epoch(model, train_loader, optimizer, criterion, device)
         val_loss, val_acc = vali(model, val_loader, criterion, labels, device)
 
-        print(f"Training:Epoch[{epoch + 1}/{MAX_EPOCH}] Train_Loss: {train_loss:.4f} Train_Acc:{train_acc:.2%}, "
+        print(f"Training:Epoch[{epoch + 1}/{args.epochs}] Train_Loss: {train_loss:.4f} Train_Acc:{train_acc:.2%}, "
               f"Val_Loss: {val_loss:.4f} Val_acc:{val_acc:.2%}")
-        if val_loss < best_val_loss:  # update model
-            torch.save(model.state_dict(), best_model_path)
+        if args.ckpt_path and val_loss < best_val_loss:  # update model
+            torch.save(model.state_dict(), args.ckpt_path)
         # model.load_state_dict(torch.load(best_model_path))
         scheduler.step()  # adjust learning rate
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='CNN family for Scene Multi-Classification')
+    # basic configs
+    parser.add_argument('--model_name', type=str, default=1, help='status')
+    parser.add_argument('--data_path', type=str, default='./train/', help='path of the data file')
+    parser.add_argument('--val_ratio', type=float, default=0.2, help='the ratio of validation set')
+    parser.add_argument('--data_augmentation', type=bool, default=False, help='whether apply data augmentation')
+    # training parameters
+    parser.add_argument('--epochs', type=int, default=1, help='max training epochs')
+    parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+    parser.add_argument('--lr', type=float, default=0.005, help='batch size')
+    parser.add_argument('--ckpt_path', type=str, default='./ckpt/', help='path for saving the best model')
+    args = parser.parse_args()
+
+    labels = {'bedroom': 1, 'Coast': 2, 'Forest': 3, 'Highway': 4, 'industrial': 5, 'Insidecity': 6,
+              'kitchen': 7, 'livingroom': 8, 'Mountain': 9, 'Office': 10, 'OpenCountry': 11, 'store': 12,
+              'Street': 13, 'Suburb': 14, 'TallBuilding': 15}
+    args.labels = dict([(k, v - 1) for k, v in labels.items()])
+    args.class_num = 15
+    args.attention_block_num = [1, 1, 1]
+    main(args)
