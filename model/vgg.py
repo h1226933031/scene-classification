8e70c8eee528bcc51ca5669e813b179a5fbbc022
LI-Yixuan <52031121+LI-Yixuan@users.noreply.github.com>
2023-04-17 15:42:58 +0800

    add vgg

---
+++ model/vgg.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+
+cfg = {
+    'Modified': [64,  'M', 128,   'M', 256,                 'M', 512, 'M', 512, 'M'],
+    'A' : [64,     'M', 128,      'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
+    'B' : [64, 64, 'M', 128, 128, 'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
+    'D' : [64, 64, 'M', 128, 128, 'M', 256, 256, 256,      'M', 512, 512, 512,      'M', 512, 512, 512,      'M'],
+    'E' : [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+}
+
+class VGG(nn.Module):
+
+    def __init__(self, features, num_class=15):
+        super().__init__()
+        self.features = make_layers(cfg[features], batch_norm=True)
+        self.classifier = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, num_class)
+        )
+
+    def forward(self, x):
+        output = self.features(x)
+        output = output.view(output.size(0), -1)
+        output = self.classifier(output)
+
+        return output
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+
+    input_channel = 1
+    for l in cfg:
+        if l == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            continue
+
+        layers += [nn.Conv2d(input_channel, l, kernel_size=3, padding=1)]
+
+        if batch_norm:
+            layers += [nn.BatchNorm2d(l)]
+
+        layers += [nn.ReLU(inplace=True)]
+        input_channel = l
+    a = nn.Sequential(*layers)
+    return a
+
+def vgg11_bn():
+    return VGG('A')
+
+def vgg13_bn():
+    return VGG('B')
+
+def vgg16_bn():
+    return VGG('D')
+
+def vgg19_bn():
+    return VGG('E')
+
+
+if __name__ == '__main__':
+    inputs = torch.randn(8, 1, 224, 224)
+    net = VGG('Modified')
+    outputs = net(inputs)
